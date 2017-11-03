1ff7dd82190b61a935fce599c8ef489a4556f184
Kai Hendry <kai.hendry@unee-t.com>
2017-11-03 08:44:30 +0800

    Tweaks [skip-ci]

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest && chmod +x /usr/local/bin/ecs-cli
-
 /usr/local/bin/ecs-cli configure -c master -p lmb-prod
 test -f aws-env.prod && source aws-env.prod
 
 envsubst < AWS-docker-compose.yml > docker-compose-bugzilla.yml
 envsubst < AWS-docker-compose-meteor.yml > docker-compose-meteor.yml
 
-# ecs-cli compose -p meteor -f docker-compose-meteor.yml service create --target-group-arn arn:aws:elasticloadbalancing:ap-southeast-1:192458993663:targetgroup/meteor/96a08bd201369039 --container-name meteor --container-port 80 --role ecsServiceRole
-# ecs-cli compose -p bugzilla -f docker-compose-bugzilla.yml service create --target-group-arn arn:aws:elasticloadbalancing:ap-southeast-1:192458993663:targetgroup/bugzilla/48050fbe08e545a3 --container-name bugzilla --container-port 80 --role ecsServiceRole
+#ecs-cli compose -p meteor -f docker-compose-meteor.yml service create --target-group-arn arn:aws:elasticloadbalancing:ap-southeast-1:192458993663:targetgroup/meteor/96a08bd201369039 --container-name meteor --container-port 80 --role ecsServiceRole
+#ecs-cli compose -p bugzilla -f docker-compose-bugzilla.yml service create --target-group-arn arn:aws:elasticloadbalancing:ap-southeast-1:192458993663:targetgroup/bugzilla/48050fbe08e545a3 --container-name bugzilla --container-port 80 --role ecsServiceRole
 
-# /usr/local/bin/ecs-cli compose -p meteor -f docker-compose-meteor.yml service up
+/usr/local/bin/ecs-cli compose -p meteor -f docker-compose-meteor.yml service up
 /usr/local/bin/ecs-cli compose -p bugzilla -f docker-compose-bugzilla.yml service up
