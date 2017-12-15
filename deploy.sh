333013daf8dffbf2a1bcfc3f14838f2ac92a90af
Kai Hendry <kai.hendry@unee-t.com>
2017-12-15 17:24:55 +0800

    Split out front end deployment script for https://github.com/unee-t/bugzilla-customisation/issues/30 [skip-ci]

--- deploy.sh
+++ deploy.sh
@@ -30,16 +30,14 @@ done
 AWS_PROFILE=lmb-$STAGE
 shift "$((OPTIND-1))"   # Discard the options and sentinel --
 
-#curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-v0.6.6 && chmod +x /usr/local/bin/ecs-cli
-/usr/local/bin/ecs-cli configure -c master -p $AWS_PROFILE -r ap-southeast-1
+if ! test -x /usr/local/bin/ecs-cli
+then
+	curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-v0.6.6 && chmod +x /usr/local/bin/ecs-cli
+fi
+
+ecs-cli configure --cluster master --region ap-southeast-1 --compose-service-name-prefix ecscompose-service-
 test -f aws-env.$STAGE && source aws-env.$STAGE
 
 envsubst < AWS-docker-compose.yml > docker-compose-bugzilla.yml
-envsubst < AWS-docker-compose-meteor.yml > docker-compose-meteor.yml
 
-# Only user in initial setup
-#ecs-cli compose -p meteor -f docker-compose-meteor.yml service create --target-group-arn arn:aws:elasticloadbalancing:ap-southeast-1:192458993663:targetgroup/meteor/96a08bd201369039 --container-name meteor --container-port 80 --role ecsServiceRole
-#ecs-cli compose -p bugzilla -f docker-compose-bugzilla.yml service create --target-group-arn arn:aws:elasticloadbalancing:ap-southeast-1:192458993663:targetgroup/bugzilla/48050fbe08e545a3 --container-name bugzilla --container-port 80 --role ecsServiceRole
-
-/usr/local/bin/ecs-cli compose -p meteor -f docker-compose-meteor.yml service up
-#/usr/local/bin/ecs-cli compose -p bugzilla -f docker-compose-bugzilla.yml service up
+/usr/local/bin/ecs-cli compose --aws-profile $AWS_PROFILE -p bugzilla -f docker-compose-bugzilla.yml service up
