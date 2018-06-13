1e95bebc0e182aa06a038374b158521a4210f861
Kai Hendry <kai.hendry@unee-t.com>
2018-06-13 15:33:25 +0800

    Script to help quickly connect to DB [skip-ci]

---
+++ db/connect.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+STAGE=dev
+
+domain() {
+	case $1 in
+		prod) echo auroradb.unee-t.com
+		;;
+		*) echo auroradb.$1.unee-t.com
+		;;
+	esac
+}
+
+show_help() {
+cat << EOF
+Usage: ${0##*/} [-p]
+
+By default, deploy to dev environment on AWS account 812644853088
+
+	-p          PRODUCTION 192458993663
+	-d          DEMO 915001051872
+
+EOF
+}
+
+while getopts "pd" opt
+do
+	case $opt in
+		p)
+			echo "PRODUCTION" >&2
+			STAGE=prod
+			;;
+		d)
+			echo "DEMO" >&2
+			STAGE=demo
+			;;
+		*)
+			show_help >&2
+			exit 1
+			;;
+	esac
+done
+AWS_PROFILE=uneet-$STAGE
+shift "$((OPTIND-1))"   # Discard the options and sentinel --
+
+echo Connecting to ${STAGE^^} $(domain $STAGE)
+
+MYSQL_PASSWORD=$(aws --profile $AWS_PROFILE ssm get-parameters --names MYSQL_PASSWORD --with-decryption --query Parameters[0].Value --output text)
+MYSQL_USER=$(aws --profile $AWS_PROFILE ssm get-parameters --names MYSQL_USER --with-decryption --query Parameters[0].Value --output text)
+
+mysql -h $(domain $STAGE) -P 3306 -u $MYSQL_USER --password=$MYSQL_PASSWORD bugzilla
