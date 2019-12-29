d3d8ec410669cabb5d40f580ceaa591fbccbdc0a
Kai Hendry <kai.hendry@unee-t.com>
2019-12-29 13:45:15 +0800

    Use MAIL_URL as this is one the Frontend uses

--- mail-test/setup-mail-to.sh
+++ mail-test/setup-mail-to.sh
@@ -14,23 +14,20 @@ then
 	exit 1
 fi
 
-USER=$(aws --profile ins-${STAGE} ssm get-parameters --names SES_SMTP_USERNAME --with-decryption --query Parameters[0].Value --output text)
-PASS=$(aws --profile ins-${STAGE} ssm get-parameters --names SES_SMTP_PASSWORD --with-decryption --query Parameters[0].Value --output text)
-SENDER=$(aws --profile ins-${STAGE} ssm get-parameters --names SES_VERIFIED_SENDER --with-decryption --query Parameters[0].Value --output text)
+getparam () {
+    aws --profile ins-${STAGE} ssm get-parameters --names "$1" --with-decryption --query Parameters[0].Value --output text
+}
+
+USER=$(getparam SES_SMTP_USERNAME)
+MAIL_URL=$(getparam MAIL_URL)
+PASS=$(getparam SES_SMTP_PASSWORD)
+SENDER=$(getparam SES_VERIFIED_SENDER)
 
 cat << END > index.js
 var nodemailer = require('nodemailer');
 
 // create reusable transporter object using the default SMTP transport
-var transporter = nodemailer.createTransport({
-    host: 'email-smtp.us-west-2.amazonaws.com',
-    port: 465,
-    secure: true, // use SSL
-    auth: {
-        user: '${USER}',
-        pass: '${PASS}'
-    }
-});
+var transporter = nodemailer.createTransport('${MAIL_URL}');
 
 // setup e-mail data with unicode symbols
 var mailOptions = {
