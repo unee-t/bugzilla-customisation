bd300f572240d1b9d472769a034819b3d1d528c1
franck <franck@unee-t.com>
2020-01-06 17:44:52 +0800

    remove hard coded variables from db/iam-test.sh

--- db/iam-test.sh
+++ db/iam-test.sh
@@ -1,17 +1,37 @@
 #!/usr/bin/env bash
 
-# We need to remove hard coded variables from there.
+#########
+# TO DO
+#########
+# We need to 
+#   - remove the hard coded IAMUSER variables from there.
+#   - make this work for the DEMO and PROD environment too.
+#     This version only works for the DEV environment.
+#     See the scripts connect.sh and describe.sh for example of it can be done
+###############################################################################
+
+# This script facilitates test of the IAM user connection to the BZ Db
+#
+# We needs several variables that are stored in the aws-env.[stage] file
+#		- STAGE
+#		- AWS_REGION
+#       - AWS_PROFILE
+#       - MYSQL_HOST
+#       - MYSQL_PORT
 
 set -x
+source aws-env.dev
 
-# https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_MariaDB.html#MariaDB.Concepts.SSLSupport
+# HARD CODED VARIABLE
+
+    IAMUSER="mydbuser"
+
+# For more details:
+    # https://aws.amazon.com/blogs/database/use-iam-authentication-to-connect-with-sql-workbenchj-to-amazon-aurora-mysql-or-amazon-rds-for-mysql/
+    # https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_MariaDB.html#MariaDB.Concepts.SSLSupport
+
 curl -O https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
 
-# https://aws.amazon.com/blogs/database/use-iam-authentication-to-connect-with-sql-workbenchj-to-amazon-aurora-mysql-or-amazon-rds-for-mysql/
-REGION="ap-southeast-1"
-AURORAEP=auroradb.dev.unee-t.com
-#AURORAEP=twoam2-cluster.cluster-c5eg6u2xj9yy.ap-southeast-1.rds.amazonaws.com
-IAMUSER="mydbuser"
+TOKEN="$(aws --profile $AWS_PROFILE rds generate-db-auth-token --hostname $MYSQL_HOST --port $MYSQL_PORT --username $IAMUSER --region=$AWS_REGION)"
 
-TOKEN="$(aws --profile uneet-dev rds generate-db-auth-token --hostname $AURORAEP --port 3306 --username $IAMUSER --region=$REGION)"
-mysql -h $AURORAEP -P 3306 --ssl-ca=./rds-combined-ca-bundle.pem --enable-cleartext-plugin --user=$IAMUSER --password=$TOKEN
+mysql -h $MYSQL_HOST -P $MYSQL_PORT --ssl-ca=./rds-combined-ca-bundle.pem --enable-cleartext-plugin --user=$IAMUSER --password=$TOKEN
